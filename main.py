6604bbf50bbc3a2dc2f26be20675fae76737bea6
陈银 <chenyin1050@163.com>
2021-05-07 23:41:19 +0800

    完成界面

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets # import PyQt5 widgets
+from PyQt5.QtWidgets import QWidget
+
+from mainwindow import Ui_MainWindow
+import sys
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())
